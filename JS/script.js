c377fb037b8bb2d7ca31e878e6137898ba93eaaa
Therese Hagwall <90thejag@skola.boras.se>
2023-01-10 15:21:45 +0100

    Lägg till allt

---
+++ JS/script.js
@@ -0,0 +1,3 @@
+"use strict";
+const inputField = document.querySelector("input");
+const addButton = document.querySelector("button");
---
+++ TS/script.ts
@@ -0,0 +1,3 @@
+const inputField = document.querySelector("input") as HTMLInputElement;
+
+const addButton = document.querySelector("button") as HTMLButtonElement;
